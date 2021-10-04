8db437f6beca8f64484da42670a1ba6c22d5882e
Thor Eric Dueholm <s194589@student.dtu.dk>
2021-10-04 13:59:30 +0200

    Solution to problem 2

--- src/Field.java
+++ src/Field.java
@@ -6,18 +6,25 @@
 
 public class Field {
 
-    public Field() {
+    Semaphore[][] semaphores = new Semaphore[Layout.ROWS][Layout.COLS];
 
+    public Field() {
+        //Instantiate a semaphore for each tile to ensure only one car is on a tile at a time.
+        for (int i = 0; i < Layout.ROWS; i++) {
+            for (int j = 0; j < Layout.COLS; j++) {
+                semaphores[i][j] = new Semaphore(1);
+            }
+        }
     }
 
     /* Block until car no. may safely enter tile at pos */
     public void enter(int no, Pos pos) throws InterruptedException {
-
+        semaphores[pos.row][pos.col].P(); //Pick up coconut or wait for it.
     }
 
     /* Release tile at position pos */
     public void leave(Pos pos) {
-
+        semaphores[pos.row][pos.col].V(); //throw coconut back
     }
 
 }
