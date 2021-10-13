adc7bd38b4b2f630e4bdb26e92878eef1ec26ba6
KristineKlok <55542051+KristineKlok@users.noreply.github.com>
2021-10-13 14:47:36 +0200

    woop

--- src/BatonAlley.java
+++ src/BatonAlley.java
@@ -6,26 +6,74 @@
 
 public class BatonAlley extends Alley {
 
+    int up, down;
+    Semaphore upSem, downSem, entry;
+
     protected BatonAlley() {
-        
+        up = 0; down = 0;
+
+        upSem   = new Semaphore(0);
+        downSem = new Semaphore(0);
+        entry   = new Semaphore(1);
     }
 
     /* Block until car no. may enter alley */
     public void enter(int no) throws InterruptedException {
         if (no < 5) {
+            entry.P();
+            if (up > 0 ){
+                down++;
+                entry.V();
+                downSem.P();
+            }
+            down ++;
+            signal();
             
         } else {
-            
+            entry.P();
+            if (down > 0){
+                up++;
+                entry.V();
+                upSem.P();
+            }
+            up++;
+            signal();
         }
 
      }
 
+    private void signal() {
+        if(up == 0 && down > 0){
+            down--;
+            downSem.V();
+        } else if (down == 0 && up > 0){
+            up--;
+            upSem.V();
+        } else{
+            entry.V();
+        }
+    }
+
     /* Register that car no. has left the alley */
     public void leave(int no) {
         if (no < 5) {
-
+            try {
+                entry.P();
+            } catch (InterruptedException e) {
+                System.out.println("shit went wrong, yo!");
+                e.printStackTrace();
+            }
+            down--;
+            signal();
         } else {
-
+            try {
+                entry.P();
+            } catch (InterruptedException e) {
+                System.out.println("shit went wrong, yo!");
+                e.printStackTrace();
+            }
+            up--;
+            signal();
         }
     }
 
